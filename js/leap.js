a636ac8a3730fcc4f09755a75f19dc3232b55670
AndGasper <andy.gasper1@gmail.com>
2017-05-23 19:16:19 -0700

    Accurate color version for leap motion worker

--- js/leap.js
+++ js/leap.js
@@ -27,7 +27,7 @@ controller.loop(function(frame) {
         var color = 'rgb(' + x + "," + z + "," + y + ')';
         sendColor(color);
 
-        changeColor(12, {"r":x, "g": z, "b": y}); // send the change color
+        changeColor(11, {"r":x, "g": z, "b": y}); // add a 500 ms delay before changing the color call
 
     }
   }
@@ -40,64 +40,101 @@ controller.loop(function(frame) {
 function sendColor(color){
     document.getElementById('box').style.backgroundColor = color;
 }
-function getHSB(r,g,b) {
-    let HSB = {
-        hue: null,
-        saturation: null,
-        brightness: null
+// function getHSB(r,g,b) {
+//     let HSB = {
+//         hue: null,
+//         saturation: null,
+//         brightness: null
+//     };
+//     // Normalize the color values out of 255
+//     let red = r/255;
+//     let green = g/255;
+//     let blue = b/255;
+//
+//     let colorMin = Math.min(red,green,blue); // get the miniumum of the adjusted color
+//     let colorMax = Math.max(red,green, blue); // Get the maximum of the adjusted color
+//     let deltaColor = colorMax - colorMin;
+//     // formula for lightness; note: lightness = brightness
+//     HSB.brightness = (colorMax+colorMin)/2;
+//     // if deltaColor = 0 then set saturation equal to 0,
+//     // else set sautration equal to (deltaColor/1-|2*brightness - 1|)
+//     (deltaColor === 0) ? (HSB.saturation = 0) : (HSB.saturation = (deltaColor / (1 - Math.abs(2*HSB.brightness-1) ) ) );
+//
+//     switch(colorMax) {
+//         case(red):
+//             //Max number 6 modulo' well not really magic
+//             HSB.hue = (60 * ( ((green - blue)/deltaColor)%6));
+//             (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
+//             return HSB; // return the now complete Hue, Saturation, and Brightness object
+//             break;
+//
+//         case(blue):
+//             HSB.hue = (60 * ( ((red-green)/deltaColor) + 4));
+//             (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
+//             return HSB;
+//             break;
+//
+//         case(green):
+//             HSB.hue = (60 * ( ((blue-red)/deltaColor) + 2));
+//             (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
+//             return HSB;
+//             break;
+//         default:
+//             console.log("Something has gone wrong if you've made it all the way here");
+//     }
+// }
+// function getHSB (r, g, b) {
+//     r /= 255, g /= 255, b /= 255;
+//
+//     var max = Math.max(r, g, b), min = Math.min(r, g, b);
+//     var h, s, v = max;
+//
+//     var d = max - min;
+//     s = max == 0 ? 0 : d / max;
+//
+//     if (max == min) {
+//         h = 0; // achromatic
+//     } else {
+//         switch (max) {
+//             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+//             case g: h = (b - r) / d + 2; break;
+//             case b: h = (r - g) / d + 4; break;
+//         }
+//
+//         h /= 6;
+//     }
+//
+//     return {hue: h, saturation: s, brightness: v };
+// }
+
+function calculate_cieXY_from_rgb(R,G,B){
+    const X = 0.4124*R + 0.3576*G + 0.1805*B;
+    const Y = 0.2126*R + 0.7152*G + 0.0722*B;
+    const Z = 0.0193*R + 0.1192*G + 0.9505*B;
+    const x = X / (X + Y + Z);
+    const y = Y / (X + Y + Z);
+    const xy = {
+        x: x,
+        y: y
     };
-    // Normalize the color values out of 255
-    let red = r/255;
-    let green = g/255;
-    let blue = b/255;
-
-    let colorMin = Math.min(red,green,blue); // get the miniumum of the adjusted color
-    let colorMax = Math.max(red,green, blue); // Get the maximum of the adjusted color
-    let deltaColor = colorMax - colorMin;
-    // formula for lightness; note: lightness = brightness
-    HSB.brightness = (colorMax+colorMin)/2;
-    // if deltaColor = 0 then set saturation equal to 0,
-    // else set sautration equal to (deltaColor/1-|2*brightness - 1|)
-    (deltaColor === 0) ? (HSB.saturation = 0) : (HSB.saturation = (deltaColor / (1 - Math.abs(2*HSB.brightness-1) ) ) );
-
-    switch(colorMax) {
-        case(red):
-            //Max number 6 modulo' well not really magic
-            HSB.hue = (60 * ( ((green - blue)/deltaColor)%6));
-            (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
-            return HSB; // return the now complete Hue, Saturation, and Brightness object
-            break;
-
-        case(blue):
-            HSB.hue = (60 * ( ((red-green)/deltaColor) + 4));
-            (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
-            return HSB;
-            break;
-
-        case(green):
-            HSB.hue = (60 * ( ((blue-red)/deltaColor) + 2));
-            (HSB.hue < 0) ? (HSB.hue += 360) : (''); // If hue is negative, flip the color to the other side of the wheel by adding 360 degrees
-            return HSB;
-            break;
-        default:
-            console.log("Something has gone wrong if you've made it all the way here");
-    }
+    return(xy);
 }
 
 function changeColor(lightNumber, color) {
     const BASE_URL_LIGHTS = `http://${bridgeIPAddress}/api/${hueUsername}/lights`;
     const {r, g, b} = color; // pull off r, g, and b from color
-    const {hue, saturation, brightness} =  getHSB(r,g, b); // pull off hue saturation and brightness from the HSB return value
+    const {x,y} =  calculate_cieXY_from_rgb(r,g,b); // pull off hue saturation and brightness from the HSB return value
 
-    let hueAdjusted = parseInt((hue/365)*65535);
-    let saturationAdjusted = parseInt(saturation*254);
-    let brightnessAdjusted = parseInt(brightness*254);
+    // let hueAdjusted = parseInt((hue/360)*65535);
+    // let saturationAdjusted = parseInt(saturation*254);
+    // let brightnessAdjusted = parseInt(brightness*254);
 
     $.ajax({
         url: `${BASE_URL_LIGHTS}/${lightNumber}/state`,
         method: "PUT",
         dataType: "JSON",
-        data: JSON.stringify({"hue": hueAdjusted, "sat": saturationAdjusted, "bri": brightnessAdjusted, "transitiontime": 0}),
+        data: JSON.stringify({"xy":[x,y], "transitiontime": 0}),
+        // data: JSON.stringify({"hue": hueAdjusted, "sat": saturationAdjusted, "bri": brightnessAdjusted, "transitiontime": 0}),
         success: (response) => {
 
         },
